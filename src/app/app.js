9d67335b4ca519448a6fb7e76aefc04163637e0e
gihooh <carloginocatapang@gmail.com>
2018-10-20 09:47:35 +0800

    Bootstrap react app

---
+++ src/app/app.js
@@ -0,0 +1,34 @@
+// The basics
+import React, { PureComponent } from 'react';
+import { withRouter } from 'react-router';
+
+// Action creators and helpers
+import Header from './components/header/Header';
+import Routes from './routes';
+
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  text-align:center;
+`
+
+const Content = styled.div`
+  margin-top: 10px;
+`
+
+class App extends PureComponent {
+  render() {
+    return (
+      <AppWrapper>
+        <Header
+          current={this.props.location.pathname}
+        />
+        <Content>
+          <Routes />
+        </Content>
+      </AppWrapper>
+    );
+  }
+}
+
+export default withRouter(App);
