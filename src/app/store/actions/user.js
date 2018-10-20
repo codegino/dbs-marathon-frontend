e923df462bcaf59c25091c871a9e00ec0e184434
gihooh <carloginocatapang@gmail.com>
2018-10-20 09:54:13 +0800

    Added user actions

---
+++ src/app/store/actions/user.js
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import {onLoadingStart, onLoadingEnd} from './ui';
+import {history} from '../../../store';
+
+export const REGISTER_USER = 'auth/REGISTER_USER';
+export const REGISTER_USER_SUCCESS = 'auth/REGISTER_SUCCESS';
+export const REGISTER_USER_FAILED = 'auth/REGISTER_FAILED';
+export const REVIEW_USER = 'auth/REVIEW/USER';
+export const LOGIN_SUCCESS = 'auth/LOGIN_SUCESS';
+
+export const reviewUser = (user) => async dispatch => {
+  dispatch({type: REVIEW_USER, user}) 
+  history.push('/review')
+}
+
+export const login = ({username, password}) => async dispatch => {
+  dispatch(onLoadingStart());
+  try {
+    const res = await axios.post('login', {
+      username,
+      password 
+    })
+    console.log(res.data)
+    dispatch({type: LOGIN_SUCCESS, admin: res.data})
+    history.push('/admin')
+  } catch(e) {
+    console.log(e.message)
+    dispatch({type: REGISTER_USER_FAILED}) 
+  }
+
+  dispatch(onLoadingEnd());
+}
+
+export const registerUser = ({email, mobile}) => async dispatch => {
+  dispatch(onLoadingStart());
+  try {
+    const res = await axios.post('users', {
+      email,
+      mobile
+    })
+    console.log(res.data)
+  } catch(e) {
+    console.log(e.message)
+    dispatch({type: REGISTER_USER_FAILED}) 
+  }
+  dispatch(onLoadingEnd());
+}
+
+export const fetchUser = ({queryString}) => async dispatch => {
+  dispatch(onLoadingStart());
+  try {
+    const res = await axios.get('users', {
+      params: {
+        queryString
+      }
+    })
+
+    console.log(res.data)
+  } catch(e) {
+    console.log(e)
+    console.log(e.message)
+    dispatch({type: REGISTER_USER_FAILED}) 
+  }
+  dispatch(onLoadingEnd());
+}
