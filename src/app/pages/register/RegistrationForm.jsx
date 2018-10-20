3c55676d2c303209ec380fa0ab2ae04c4134e823
gihooh <carloginocatapang@gmail.com>
2018-10-20 09:57:21 +0800

    Added registration form

---
+++ src/app/pages/register/RegistrationForm.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Page from '../../components/page';
+import { reviewUser } from '../../store/actions/user';
+import styled from 'styled-components';
+
+const containerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+}
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-self: center;
+  border: 1px solid black;
+  border-radius: 6px;
+  width: 90%;
+  padding: 5px 1rem;
+`
+
+class RegistrationPage extends React.PureComponent {
+  state = {
+    mobile: '',
+    email: ''
+  }
+
+  render() {
+    const {email, mobile} = this.state
+    return (
+      <Page id="login" title="Login" description="Register to marathon." style={containerStyle}>
+        <Form onSubmit={e => e.preventDefault()}>
+          <label htmlFor="email">Email</label>
+          <input id="email" type="text" placeholder="Enter email address" value={email}
+          onChange={e => this.setState({email: e.target.value})}/>
+          <label htmlFor="mobile">Mobile</label>
+          <input id="mobile" type="text" placeholder="Enter mobile number" value={mobile}
+          onChange={e => this.setState({mobile: e.target.value})}/>
+          <input type="button" onClick={() => this.props.reviewUser({email, mobile})} value="Submit"/>
+        </Form>
+      </Page>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  reviewUser: (user) => dispatch(reviewUser(user))
+})
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(RegistrationPage);
